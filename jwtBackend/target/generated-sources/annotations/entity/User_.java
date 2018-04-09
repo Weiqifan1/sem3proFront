d7fa5b9e9601bd41033866328bfc860f3e30c3bc
Bo Henriksen <bomarconi@gmail.com>
2018-04-09 13:52:15 +0200

    Secure backend

---
+++ jwtBackend/target/generated-sources/annotations/entity/User_.java
@@ -0,0 +1,17 @@
+package entity;
+
+import entity.Role;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-05T18:09:53")
+@StaticMetamodel(User.class)
+public class User_ { 
+
+    public static volatile SingularAttribute<User, String> userPass;
+    public static volatile SingularAttribute<User, String> userName;
+    public static volatile ListAttribute<User, Role> roleList;
+
+}
