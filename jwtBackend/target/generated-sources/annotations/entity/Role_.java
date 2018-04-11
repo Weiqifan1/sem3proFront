07aee364000ead854b3fb7c09332a562aef794b1
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-04-11 14:56:01 +0200

    Nu virker vores remoteendpoint

--- jwtBackend/target/generated-sources/annotations/entity/Role_.java
+++ jwtBackend/target/generated-sources/annotations/entity/Role_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-10T16:40:43")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-04-11T14:40:22")
 @StaticMetamodel(Role.class)
 public class Role_ { 
 
