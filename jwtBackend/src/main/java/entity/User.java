2839bf38e649a531ce079cf1d2171d234e30186d
walter01git <26571249+walter01git@users.noreply.github.com>
2018-04-12 11:15:03 +0200

    Javadoc to files

--- jwtBackend/src/main/java/entity/User.java
+++ jwtBackend/src/main/java/entity/User.java
@@ -15,9 +15,18 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import org.mindrot.jbcrypt.BCrypt;
 
+/**
+ * 
+ * @author Ticondrus
+ * 
+ * Javadoc: User.java is a class that do getters and setters plus holds or update the userdata in a list created from the database -
+ * or the list, whether it is a user creation or user login.
+ */
+
 @Entity
 @Table(name = "users")
 public class User implements Serializable {
+    
 
     private static final long serialVersionUID = 1L;
     @Id
