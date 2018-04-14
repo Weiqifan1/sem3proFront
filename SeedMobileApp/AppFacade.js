993c36195c88a60fd888477923f4327a9c905073
BenedikteEva <cph-bm109@cphbusiness.dk>
2018-04-14 12:24:04 +0200

    troubleshooting

--- SeedMobileApp/AppFacade.js
+++ SeedMobileApp/AppFacade.js
@@ -5,70 +5,87 @@ import AsyncStorage from 'react-native'
 
 const URL = "https://benedikteeva.dk/jwtBackend%2D1.0%2DSNAPSHOT";
 
-function handleHttpErrors(res) {
+async function handleHttpErrors(res) {
     if (!res.ok) {
-        throw { message: res.statusText, status: res.status };
+     throw { message: res.statusText, status: res.status };
     }
-    return res.json();
+    console.log(res.json)
+    return await res.json();
 }
 
 class AppFacade {
 
-    fetchData = () => {
-        const options = this.makeFetchOptions("GET");
-        return fetch(URL + "/api/info/user", options).then(handleHttpErrors);
+    fetchData = async () => {
+     const options =  await this.makeFetchOptions("GET");
+     return fetch(URL + "/api/info/user", options).then(handleHttpErrors);
     }
 
     setToken = async (token) => {
         try {
             await AsyncStorage.setItem('jwtToken', token)
         } catch(error){
-            alert("in setToken  "+error)
+            console.log("in setToken  ",error)
         }
     }
 
     getToken =async () => {
-        try {
-          await AsyncStorage.getItem('jwtToken');
-        } catch(error){
-            alert("getToken  "+error)
+      try {
+        const value = await AsyncStorage.getItem('jwtToken');
+        if (value !== null){
+            // We have data!!
+            console.log(value);
+            return value;
+          }
+        } catch (error) {
+          console.log(error, "value = null")
         }
     }
-
-    loggedIn = () => {
-        const loggedIn = this.getToken() != null;
+  
+    
+    loggedIn = async() => {
+        console.log("når vi til loggedin?")
+        const loggedIn =await this.getToken() != null;
         return loggedIn;
     }
     logout = async () => {
         try {
             await AsyncStorage.removeItem('jwtToken');
         } catch(error){
-            alert("logout "+error)
+            console.log("logout ",error)
         }
     }
 
-    login = (user, pass) => {
-
-        const options = this.makeFetchOptions("POST", { username: user, password: pass });
+    login = async(user, pass) => {
+        console.log(user, " Du er nu nået til facade.login")
+//nu er der hul igennem godt gjort man skal til at tænke selv 
+        const options = await this.makeFetchOptions("POST", { username: user, password: pass });
         return fetch(URL + "/api/login", options, true)
+        
             .then(handleHttpErrors)
-            .then(res => { this.setToken(res.token) })
+
+            .then(res => { 
+                console.log(res, "hgjhghjghjgh")
+                this.setToken(res.token) })
+                .then(res => this.setState({ loggedIn: true }));
 
 
     }
 
-    makeFetchOptions = (type, b) => {
+    makeFetchOptions = async (type, userInfo) => {
+        console.log(" du er i makefetchoptions "+type+" b "+userInfo.username)
         let headers = {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         }
         if (this.loggedIn()) {
-            headers["x-access-token"] = this.getToken();
+            console.log("her kommer det spændende med gettoken")
+            headers["x-access-token"] =this.getToken();
         }
         return {
             method: type,
             headers,
-            body: JSON.stringify(b)
+            body: JSON.stringify(userInfo)
+            
         }
     }
 }
