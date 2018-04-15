65e2fb3a6068db35e902c88213d3351ae85a3fd2
Bo Henriksen <bomarconi@gmail.com>
2018-04-15 12:10:53 +0200

    Url i frontend er sat til Benediktes backend.

--- frontend/src/apiFacade.js
+++ frontend/src/apiFacade.js
@@ -1,7 +1,7 @@
 import jwtDecode from 'jwt-decode';
 
-
-const URL = "http://localhost:8084/jwtbackend";
+//const URL = "http://localhost:8084/jwtbackend";
+const URL = "https://benedikteeva.dk/jwtBackend%2D1.0%2DSNAPSHOT";
 
 function handleHttpErrors(res) {
     if (!res.ok) {
